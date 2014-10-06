b1e7a02905c5a6237f8b7d472c1b05fd95688d18
AreYouCereal <Wendi@CreateSalesAndMarketing.com>
2014-10-06 10:45:49 -0500

    JS Favorite Books

---
+++ JavaScriptExercises/FavoriteBooks.js
@@ -0,0 +1,12 @@
+
+var favoriteBooks = [
+  {
+    title: "JavaScript Sucks",
+    author: "Wendi Speciale"
+  },
+  {   title: "To Kill a Mockingbird",
+    author: "Harper Lee"
+}
+];
+
+console.table(favoriteBooks);
